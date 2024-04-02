302c4a1c999e322f8e408b7fef14f679591d20f6
ackimixs <ackimixs@protonmail.com>
2024-04-02 21:00:20 +0200

    remove somethings

--- wrapper.cpp
+++ wrapper.cpp
@@ -1,12 +1,8 @@
 #include "wrapper.h"
 
 void write_2_arduino(serialib* serial, const char* message){
-    char myString[MAX_MESSAGE_LEN] = {0};
-    strcpy(myString, message);
-    myString[strlen(myString)] = '\0';
-
-    std::cout << "Writing to arduino: " << myString << std::endl;
-    int t = serial->writeString(myString);
+    // std::cout << "Writing to arduino: " << myString << std::endl;
+    int t = serial->writeString(message);
 
     std::cout << "Result : " << t << std::endl;
 }
