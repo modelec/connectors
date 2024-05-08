23c8f5951e58cfb2e01240a2a3f5a1096ca67957
ackimixs <ackimixs@protonmail.com>
2024-05-08 15:58:29 +0200

    Log

--- MyTCPClient.cpp
+++ MyTCPClient.cpp
@@ -108,7 +108,7 @@ void MyTCPClient::handleMessageFromArduino(const std::string &message) {
         waitForPong = false;
     } else {
         try {
-            std::cout << "Received from arduino : " << message << std::endl;
+            // std::cout << "Received from arduino : " << message << std::endl;
             std::vector<std::string> args = TCPSocket::split(message, ":");
             if (args.size() == 2) {
                 if (transitMode.waitingFor2 && TCPSocket::startWith(args[1], "2")) {
@@ -147,7 +147,7 @@ void MyTCPClient::handleMessageFromArduino(const std::string &message) {
                     this->robotPose.theta = std::stof(token[2]) / 100;
                 }
             }
-            std::cout << "New position : " << this->robotPose.pos.x << " " << this->robotPose.pos.y << " " << this->robotPose.theta << std::endl;
+            // std::cout << "New position : " << this->robotPose.pos.x << " " << this->robotPose.pos.y << " " << this->robotPose.theta << std::endl;
         } catch (const std::exception& ex) {
             std::cerr << "Error parsing message from arduino : " << ex.what() << std::endl;
         }
